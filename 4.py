3226b2951acc27ae2e86e5cf30f61bcb0b7954ba
anurag kumar <77048227+keshri29@users.noreply.github.com>
2023-03-14 19:15:22 +0530

    uploaded program 4

---
+++ 4.py
@@ -0,0 +1,9 @@
+#function
+#wafunction which takes a list of names as argument and greets "hello" to everyone
+def greet(names):
+    for name in names:
+            print('hello', name)
+names = ["anurag", "anupam", "amuman", "anchal","hulle"]
+greet(names)
+
+
