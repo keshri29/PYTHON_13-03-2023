f9e84ea7bc4594de1b74f4a73799664f141f3b1f
anurag kumar <77048227+keshri29@users.noreply.github.com>
2023-03-30 15:00:01 +0530

    uploaded

---
+++ 7b.py
@@ -0,0 +1,7 @@
+#experiment number 2.b
+a=float(input("enter the value of a:"))
+b=float(input("enter the value of b:"))
+c=float(input("enter the value of c:"))
+s=(a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print("the area of triangle is",area)
