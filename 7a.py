d0bb68078b941898f165ac772355c5d27786c398
anurag kumar <77048227+keshri29@users.noreply.github.com>
2023-03-30 14:57:41 +0530

    uploaded

---
+++ 7a.py
@@ -0,0 +1,14 @@
+#experiment number 2.a
+import math
+x1=int(input("enter the value of x1:"))
+x2=int(input("enter the value of x2:"))
+y1=int(input("enter the value of y1:"))
+y2=int(input("enter the value of y2:"))
+ans=math.sqrt(((x2 - x1 ))+ ((y2-y1))**0.5)
+# ans= ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5)
+print("distance between two point",(x1,x2),"and",(y1,y2),"is : ",ans)
+
+
+
+
+
